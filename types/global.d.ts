eeeea075f292be88efd0844b4325505be9d4472d
AdekunleBamzz <bams.kunle@gmail.com>
2025-05-10 08:41:01 +0100

    fix: add disconnect and isConnected types to window.ethereum

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,5 +4,7 @@ interface Window {
     request: (args: { method: string; params?: any[] }) => Promise<any>;
     on: (eventName: string, handler: (...args: any[]) => void) => void;
     removeListener: (eventName: string, handler: (...args: any[]) => void) => void;
+    disconnect?: () => Promise<void>;
+    isConnected?: () => boolean;
   };
 } 
